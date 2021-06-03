6e9448c098f2da357b62fb6e0f81f52c02eaf6e5
jignesh87 <j.mca25@gmail.com>
2021-06-03 18:18:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import './css/custom.css';
-import 'antd/dist/antd.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Login from './Pages/Login';
-import Home from './Pages/Layout';
-
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Index from './Layout/Index';
+import Login from './components/Login';
+import useToken from './components/useToken';
 
 function App() {
-  let token = false;
- 
+
+  const { token, setToken } = useToken();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
   return (
-    <div className="App">
-      <Router>
-        {
-          token
-          ? 
-            <Home />
-          :
-            <Login /> 
-        }
-      </Router> 
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Index />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
